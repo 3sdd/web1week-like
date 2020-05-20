105548f4b29017168ab367b2bf366f4ff3179f87
3sdd <56486103+3sdd@users.noreply.github.com>
2020-05-21 00:06:51 +0900

    アイコン表示された

--- src/components/LikeComponent.vue
+++ src/components/LikeComponent.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
-        like
+        <div class="mb-4">
+            <span class="num">0</span>
+        </div>
+        <div>
+            <Octicon :icon="thumbsup" :scale="15" class="icon"/>
+        </div>
     </div>
 </template>
 
 <script>
+//heart-outline
+//heart
+//thumbsup
+import Octicon,{thumbsup} from "octicons-vue"
+
 export default {
-    
+    components:{
+        Octicon
+    },
+    data:function(){
+        return{
+            thumbsup
+        }
+    }
 }
 </script>
 
 <style scoped>
+.icon{
+    width: 100px;
+    height: 100px;
+}
+
+.num{
+    font-size: 200px;
+}
 
 </style>
